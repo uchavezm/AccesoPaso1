dd59233a784988d8d22d3ce084744a03c26179dd
uchavezm <86226411+uchavezm@users.noreply.github.com>
2021-06-29 22:40:03 -0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AccesoPaso1.Startup))]
+namespace AccesoPaso1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
